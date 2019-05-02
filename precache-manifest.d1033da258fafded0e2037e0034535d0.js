9e306c0bfb4654aa3e7940cc0043de921324dc35
Willem-Aart van Dorpen <mail@willem-aart.nl>
2019-05-02 13:25:50 +0200

    Updates

---
+++ precache-manifest.d1033da258fafded0e2037e0034535d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d8fccc4c5e57f73968d738cfd78a44",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "2b17ddcad11ad513852b",
+    "url": "/cv/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cv/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2b17ddcad11ad513852b",
+    "url": "/cv/static/js/main.dcd8f01d.chunk.js"
+  },
+  {
+    "revision": "18079a4bdaee1d56978c",
+    "url": "/cv/static/js/runtime~main.6f651ed7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
